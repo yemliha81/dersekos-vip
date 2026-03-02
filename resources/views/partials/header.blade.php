59953124a12b96c0582880557c43dfed9bb8c0b0
yemliha81 <yemlihaddln@hotmail.com>
2026-03-02 14:33:47 +0300

    Favicon updated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <!-- favicon --> 
-    <link rel="icon" type="image/png" href="" />
+    <link rel="icon" type="image/ico" href="{{ asset('dersekos-vip-favicon.png') }}" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=SN+Pro:ital,wght@0,200..900;1,200..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
