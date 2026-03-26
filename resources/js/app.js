c74c1cd780b268e5aec80edf472c22f793ac6cb9
yemliha81 <yemlihaddln@hotmail.com>
2026-03-26 08:45:20 +0300

    Zoom rollback

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ZoomMeeting from "./ZoomMeeting";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <React.StrictMode>
+        <ZoomMeeting />
+    </React.StrictMode>
+);
